d2cc1e51c49cee777315757ac3d9ee98a6529aaa
Xiaojun Huang <xhuang@2-A-3.42heilbronn.de>
2025-01-14 19:33:46 +0100

    minishell start

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,15 +6,62 @@
 /*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:53:04 by xhuang            #+#    #+#             */
-/*   Updated: 2025/01/14 17:06:36 by xhuang           ###   ########.fr       */
+/*   Updated: 2025/01/14 19:32:32 by xhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "../libft/libft.h"
+# include "../libft/libft.h"
+# include <stdbool.h>
 
-void test_function(int i);
+typedef struct s_token
+{
+	char			*input;
+	char			*input_backup;
+	bool			var_check;
+	int				type;
+	int				status;
+	bool			joined;
+	struct s_token	*prev;
+	struct s_token	*next;
+}					t_token;
+
+typedef struct s_pipe
+{
+	char			*infile;
+	char			*outfile;
+	char			*heredoc_delimiter;
+	bool			heredoc_quotes;
+	int				fd_in;
+	int				fd_out;
+	int				stdin_backup;
+	int				stdout_backup;
+}					t_pipe;
+
+typedef struct s_cmd
+{
+	char			*cmd;
+	char			*cmd_path;
+	char			**arg;
+	bool			pipe_out;
+	int				*pipe_fd;
+	t_pipe			*pipe;
+	struct s_cmd	*next;
+	struct s_cmd	*prev;
+}					t_cmd;
+
+typedef struct s_ms_data
+{
+	char			**envp;
+	t_cmd			*command;
+	t_token			*token;
+	pid_t			pid;
+	char			*input;
+	bool			interaction;
+	char			*working_dir;
+	char			*old_working_dir;
+}					t_ms_data;
 
 #endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,21 +6,28 @@
 /*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:53:00 by xhuang            #+#    #+#             */
-/*   Updated: 2025/01/14 18:26:05 by xhuang           ###   ########.fr       */
+/*   Updated: 2025/01/14 19:33:03 by xhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	test_function(int i)
+int	main(int argc, char **argv, char **envp)
 {
-	ft_printf("The integer is %d\n", i);
-}
+	t_ms_data	minishell;
 
-int	main(void)
-{
-	test_function(2);
-	test_function(2);
-	ft_printf("the string is %s\n", "Hello");
-	return (0);
+	ft_memset(&minishell, 0, sizeof(t_ms_data));
+	if (check_input(&minishell, argc, argv) || init_ms())
+	{
+		if (minishell.interaction)
+		{
+			mini_interact();
+		}
+		else
+		{
+			mini_nointeract();
+		}
+		// terminiate(&minishell, /*global*/);
+	}
+	terminiate(NULL, EXIT_FAILURE);
 }
