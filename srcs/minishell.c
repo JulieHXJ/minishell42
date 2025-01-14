7b7525ec6ab80889248123ddbfa7b6b3d43f631b
Xiaojun Huang <xhuang@2-A-3.42heilbronn.de>
2025-01-14 19:00:07 +0100

    files

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:53:00 by xhuang            #+#    #+#             */
-/*   Updated: 2025/01/14 17:08:09 by xhuang           ###   ########.fr       */
+/*   Updated: 2025/01/14 18:26:05 by xhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	test_function(int i)
 
 int	main(void)
 {
+	test_function(2);
 	test_function(2);
 	ft_printf("the string is %s\n", "Hello");
 	return (0);
